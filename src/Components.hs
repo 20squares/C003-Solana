4414c5c66abd1e3259e9386198432a67922a61c8
Philipp Zahn <philipp.m.zahn@gmail.com>
2023-05-31 11:26:08 +0200

    Added more components; bit of restructuring

--- src/Components.hs
+++ src/Components.hs
@@ -16,6 +16,73 @@ import OpenGames.Preprocessor
 Describes the basic available actions for actors in the market
 -}
 
+------------------
+-- Pool operations
+
+-- Add liquidity
+addLiquidity name actionSpace = [opengame|
+
+    inputs    :  state ;
+    feedback  :   ;
+
+    :---------------------------:
+
+    inputs    :  state ;
+    feedback  :   ;
+    operation :  dependentDecision name actionSpace ;
+    outputs   :  addedLiquidity ;
+    returns   :  payments ;
+
+    :---------------------------:
+
+    outputs   :  addedLiquidity ;
+    returns   :  payments ;
+  |]
+
+-- Remove liquidity
+removeLiquidity name actionSpace = [opengame|
+
+    inputs    :  state ;
+    feedback  :   ;
+
+    :---------------------------:
+
+    inputs    :  state ;
+    feedback  :   ;
+    operation :  dependentDecision name actionSpace ;
+    outputs   :  removedLiquidity ;
+    returns   :  payments ;
+
+    :---------------------------:
+
+    outputs   :  removedLiquidity ;
+    returns   :  payments ;
+  |]
+
+-- Remove liquidity
+swap name actionSpace = [opengame|
+
+    inputs    :  state ;
+    feedback  :   ;
+
+    :---------------------------:
+
+    inputs    :  state ;
+    feedback  :   ;
+    operation :  dependentDecision name actionSpace ;
+    outputs   :  swapped ;
+    returns   :  payments ;
+
+    :---------------------------:
+
+    outputs   :  swapped ;
+    returns   :  payments ;
+  |]
+
+
+---------------------
+-- Trading operations
+
 -- Open position
 openPosition name actionSpace = [opengame|
 
@@ -57,31 +124,8 @@ closePosition name actionSpace = [opengame|
     returns   :  payments ;
   |]
 
-
--- Add liquidity
-addLiquidity name actionSpace = [opengame|
-
-    inputs    :  state ;
-    feedback  :   ;
-
-    :---------------------------:
-
-    inputs    :  state ;
-    feedback  :   ;
-    operation :  dependentDecision name actionSpace ;
-    outputs   :  addedLiquidity ;
-    returns   :  payments ;
-
-    :---------------------------:
-
-    outputs   :  addedLiquidity ;
-    returns   :  payments ;
-  |]
-
-
-  
--- Remove liquidity
-removeLiquidity name actionSpace = [opengame|
+-- Add collateral
+addCollateral name actionSpace = [opengame|
 
     inputs    :  state ;
     feedback  :   ;
@@ -91,18 +135,60 @@ removeLiquidity name actionSpace = [opengame|
     inputs    :  state ;
     feedback  :   ;
     operation :  dependentDecision name actionSpace ;
-    outputs   :  removedLiquidity ;
+    outputs   :  addedCollateral ;
     returns   :  payments ;
 
     :---------------------------:
 
-    outputs   :  removedLiquidity ;
+    outputs   :  addedCollateral ;
     returns   :  payments ;
   |]
 
+ -- Remove collateral
+removeCollateral name actionSpace = [opengame|
+
+    inputs    :  state ;
+    feedback  :   ;
+
+    :---------------------------:
+
+    inputs    :  state ;
+    feedback  :   ;
+    operation :  dependentDecision name actionSpace ;
+    outputs   :  removedCollateral ;
+    returns   :  payments ;
+
+    :---------------------------:
+
+    outputs   :  removedCollateral ;
+    returns   :  payments ;
+  |]
+
+ -- Liquidate position
+liquidatePosition name actionSpace = [opengame|
+
+    inputs    :  state ;
+    feedback  :   ;
+
+    :---------------------------:
+
+    inputs    :  state ;
+    feedback  :   ;
+    operation :  dependentDecision name actionSpace ;
+    outputs   :  liquidatedPosition ;
+    returns   :  payments ;
+
+    :---------------------------:
+
+    outputs   :  liquidatedPosition ;
+    returns   :  payments ;
+  |]
+
+---------------------------
+-- General state operations
 
 -- Update state
-stateUpdateFunction name stateUpdateMechanism = [opengame|
+updateState name stateUpdateMechanism = [opengame|
 
     inputs    :  state, action ;
     feedback  :   ;
