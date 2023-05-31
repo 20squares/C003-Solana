235280fc52cee166741fa779868a9aa365c3dd6e
Philipp Zahn <philipp.m.zahn@gmail.com>
2023-05-31 15:31:31 +0200

    Basic model scenarios

--- src/Model.hs
+++ src/Model.hs
@@ -10,5 +10,6 @@ import OpenGames.Engine.Engine
 import OpenGames.Preprocessor
 
 {-
-Contains the full model
+Contains the basic state change logic for an individual user.
+By instantiating a initial state, the incentives underlying each individual for any scenario can be checked.
 -}
