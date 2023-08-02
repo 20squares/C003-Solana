7e3521ff221c968f3ffeff6436313e8a70a276f7
dpl0a <danielepalombi@protonmail.com>
2023-08-02 17:13:49 +0200

    test actionspaces

--- src/ActionSpaces.hs
+++ src/ActionSpaces.hs
@@ -1,11 +1,12 @@
 module ActionSpaces where
 
 import Types
+import Mock
 
 {-
 Describes the action space that the players have access to
 -}
 
 actionSpaceAddLiquidity, actionSpaceRemoveLiquidity :: [(PoolName, AssetName, AssetQuantity, AssetQuantity)]
-actionSpaceAddLiquidity = undefined
-actionSpaceRemoveLiquidity = undefined
+actionSpaceAddLiquidity = [("Test", "Sol", 10, 1), ("Test", "Sol", 0, 100)]
+actionSpaceRemoveLiquidity = [("Test", "Sol", 909089.86, 1), ("Test", "Sol", 0, 100)]
