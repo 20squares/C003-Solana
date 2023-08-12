82816a0a37346cc99a99929894bb843854c6d602
Fabrizio Romano Genovese <fabrizio@20squares.xyz>
2023-08-12 13:59:14 +0200

    Documentation updated.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import ActionSpaces
 import Analytics
-import Mock
+import Parameterization
 import Strategies
 
 testStrategy1 = strategyTupleAddRemoveLiquidity ("Test", "Sol", 10, 1) ("Test", "Sol", 909089.86, 1)
@@ -14,7 +14,7 @@ testStrategy4 = strategyTupleSwap ("Pool1", "Sol", "Doge", 10000000, 0) ("Pool2"
 
 main = do
   -- printEquilibriumExampleAddRemoveLiquidity state1 "Aki" (const actionSpaceAddLiquidity) (const actionSpaceRemoveLiquidity) testStrategy1
-  -- printEquilibriumExampleAddRemoveLiquidity2 state1 "Aki" (const actionSpaceAddLiquidity) (const actionSpaceRemoveLiquidity) 8 testStrategy1
+  printEquilibriumExampleAddRemoveLiquidity2 state1 "Aki" (const actionSpaceAddLiquidity) (const actionSpaceRemoveLiquidity) 3.965 testStrategy1
   -- printEquilibriumExampleSwap state3 "Aki" "Bogdan" "Pool1" "Pool2" (const actionSpaceSwap) (const $ fst actionSpaceArb, const $ snd actionSpaceArb) testStrategy3
-  printEquilibriumExampleSwapExogenousPriceChange state3 "Aki" "Bogdan" "Pool1" "Pool2" (const actionSpaceSwap) (const $ fst actionSpaceArb, const $ snd actionSpaceArb) 8 "Sol" 0.9 testStrategy3
+  -- printEquilibriumExampleSwapExogenousPriceChange state3 "Aki" "Bogdan" "Pool1" "Pool2" (const actionSpaceSwap) (const $ fst actionSpaceArb, const $ snd actionSpaceArb) 8 "Sol" 0.9 testStrategy3
   
